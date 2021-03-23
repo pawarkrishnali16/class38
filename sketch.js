6a4c7e88eafba440f4fc7bad561365d509211487
pawarkrishnali16 <76470734+pawarkrishnali16@users.noreply.github.com>
2021-03-23 15:14:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+var gameState = 0;
+var playerCount;
+var form, player, game;
+var allPlayers;
+var distance = 0;
+var database;
+var car1, car2, car3, car4, cars;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(displayWidth-20,displayHeight-20);
+
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+    if (playerCount === 4){
+        game.update(1);
+    }
+    if (gameState === 1){
+        clear();
+        game.play();
+    }
+}
+
